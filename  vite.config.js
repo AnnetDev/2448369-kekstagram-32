a81faf333bec0f88dadfa1954facac0d103264ab
Anna <annabaidikova92@gmail.com>
2025-03-02 10:23:24 +0100

    test 2

---  vite.config.js
+++  vite.config.js
@@ -2,19 +2,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/2448369-kekstagram-32/' : './',
-  build: {
-    rollupOptions: {
-      input: 'index.html',
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-      }
-    }
-  }
 }));
-
-// import { defineConfig } from 'vite';
-
-// export default defineConfig(({ command }) => ({
-//   base: command === 'build' ? '/2448369-kekstagram-32/' : './',
-// }));
-
