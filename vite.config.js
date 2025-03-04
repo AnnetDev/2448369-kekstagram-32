724b42d4b46d0c2b21fd29ec1d0c5d990efd35a8
Anna <annabaidikova92@gmail.com>
2025-03-04 09:58:21 +0100

    deploy test

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+
   css: {
     devSourcemap: true
   },
-  publicDir: 'public',
-  root: './source',
+  root: './',
   build: {
-    outDir: '../dist',
+    outDir: './dist',
   },
   base: '/2448369-kekstagram-32/',
-  server: {
-    port: 3000,
-  }
+  // server: {
+  //   port: 3000,
+  // }
 });
