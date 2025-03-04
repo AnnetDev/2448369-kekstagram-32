409ba85cbd2ca1b0deef1aba8bb3bf7f936e4ce2
Anna <annabaidikova92@gmail.com>
2025-03-04 10:47:52 +0100

    deploy test

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ import { setFormSubmit, closeUploadInput } from './form_utils.js';
 import { getData } from './api.js';
 import { showDataError } from './util.js';
 import { initFilters } from './filter.js';
+import '../vendor/pristine/pristine.min.js';
+import '../vendor/nouislider/nouislider.js';
+
 
 getData()
   .then((pictures) => {
