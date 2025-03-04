5802869eae1f417d29cfb33dbe12219e556b1975
Anna <annabaidikova92@gmail.com>
2025-03-04 10:55:52 +0100

    deploy test

--- js/form_utils.js
+++ js/form_utils.js
@@ -2,6 +2,8 @@ import { resetScale } from './scale';
 import { setEffect, resetSlider } from './effects';
 import { sendData } from './api.js';
 // import Pristine from '../vendor/pristine/pristine.min.js';
+import '../vendor/pristine/pristine.min.js';
+
 
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const publishButtonText = {
